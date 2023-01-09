69d6110d113da2a41e9e0df7fcaf05acaf063be9
Aaron Ogle <geekgonecrazy@users.noreply.github.com>
2023-01-09 12:46:33 -0600

    Fix the errors that constantly plague us while we start up o.O

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -3,10 +3,10 @@ local catppuccin = require("catppuccin")
 catppuccin.setup {}
 vim.cmd[[colorscheme catppuccin]]
 
-require'nvim-treesitter.configs'.setup { ensure_installed = "maintained", highlight = { enable = true } }
+require'nvim-treesitter.configs'.setup { ensure_installed = { 'python', 'javascript', 'lua', 'rust', 'c', 'go', 'typescript' }, highlight = { enable = true } }
 
 -- Native LSP Setup
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local lspconfig = require('lspconfig')
 
 local map = vim.api.nvim_set_keymap
